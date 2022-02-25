81e580c4a16a8489fd3b08caa4c2ce8d5996e9ce
moloch291 <danielkomjati1@gmail.com>
2022-02-25 16:37:43 +0100

    finalize

--- scripts/port_forward.py
+++ scripts/port_forward.py
@@ -1,13 +1,16 @@
-# This script only exposes a single pod via port-forwarding!
+# Usage:
+# The first cl arg should be the mode (pod, replicationcontroller, etc)
+# Second should be the name
+# Third is the namespace
 from subprocess import PIPE, run
 import sys
 import os
 
 
 def evaluate_pods(pods_info):
-    if sys.argv[1] in pods_info:
+    if sys.argv[2] in pods_info:
         os.system(
-            f"kubectl port-forward {sys.argv[3]}/{sys.argv[1]} 8080:8080 --namespace={sys.argv[2]}"
+            f"kubectl port-forward {sys.argv[1]}/{sys.argv[2]} 8080:8080 --namespace={sys.argv[3]}"
         )
         print("\nPort-forwarding cancelled...")
         exit(0)
@@ -23,7 +26,7 @@ def get_output(command):
 
 def main():
     pods_info = get_output(
-        f"kubectl get {sys.argv[3]}s --namespace={sys.argv[2]}"
+        f"kubectl get {sys.argv[1]}s --namespace={sys.argv[3]}"
     )
     evaluate_pods(pods_info)
 
